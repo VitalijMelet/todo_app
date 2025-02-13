5b0317de5b20a139d361c0839c9dc73c5a03714b
Vitalii Mieliet <vitalij.melet@gmail.com>
2025-02-13 12:09:56 +0200

    Added shadow on TextField

--- lib/main.dart
+++ lib/main.dart
@@ -108,57 +108,73 @@ class _ToDoState extends State<ToDo> {
         padding: EdgeInsets.fromLTRB(16, 24, 16, 16),
         child: Column(
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: TextField(
-                    maxLines: 3,
-                    style: TextStyle(fontSize: 16.0),
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'What next To-Do?',
-                      hintStyle:
-                          TextStyle(color: Color(0xFFc2c2c2), fontSize: 16.0),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusColor: Colors.white,
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                ElevatedButton(
-                  onPressed: isButtonActive
-                      ? () {
-                          _addToDo();
-                        }
-                      : null,
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff007EA7),
-                      minimumSize: Size(42, 40),
-                      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 2),
+            Container(
+              padding: EdgeInsets.only(bottom: 24),
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          color: Colors.blueGrey.shade200, width: 2))),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: Material(
+                      elevation: 3,
+                      shadowColor: Colors.black,
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16)),
-                      elevation: 5),
-                  child: Icon(
-                    Icons.add,
-                    color: Colors.white,
-                    size: 30,
+                      child: TextField(
+                        maxLines: 3,
+                        style: TextStyle(fontSize: 16.0),
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: 'What next To-Do?',
+                          hintStyle: TextStyle(
+                              color: Color(0xFFc2c2c2), fontSize: 16.0),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusColor: Colors.white,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 20),
+                        ),
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Align(
+                    child: ElevatedButton(
+                      onPressed: isButtonActive
+                          ? () {
+                              _addToDo();
+                            }
+                          : null,
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff007EA7),
+                          minimumSize: Size(42, 40),
+                          padding:
+                              EdgeInsets.symmetric(vertical: 2, horizontal: 2),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          elevation: 3),
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
 // Список задач
             Expanded(
